fbdcb51867255cb2ca505135c9d58a2e6607ad01
Layanza14-meet <109899454+Layanza14-meet@users.noreply.github.com>
2022-07-28 13:30:39 +0300

    update

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -8,8 +8,26 @@ app = Flask(  # Create a flask app
 )
 
 # Your code should be below
+@app.route('/')
 
+def home():
+    return render_template("home.html")
 
+@app.route('/product')
+
+def product():
+    return render_template("product.html")
+
+@app.route('/sony')
+def pro():
+    return render_template("sony.html")
+
+@app.route('/cart')
+def prl():
+    return render_template("cart.html")
+@app.route('/dji')
+def prw():
+    return render_template("dji.html")
 
 
 # Your code should be above
